a7cc2e2776273273c482fff607b86f352e470774
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-05 00:30:20 +0530

    changed

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -412,206 +412,206 @@ resource "aws_eks_addon" "ebs-csi-driver" {
 ######################################################################################################################################
 # Data sources for existing VPC components
 
-#####################################################################################################################
+# #####################################################################################################################
 
-data "aws_vpc" "existing_vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["bsp-vpc-${var.env}"]
-  }
-}
+# data "aws_vpc" "existing_vpc" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["bsp-vpc-${var.env}"]
+#   }
+# }
 
-data "aws_subnet" "private_subnet_az1" {
-  filter {
-    name   = "tag:Name"
-    values = ["bsp-private-subnet-az1-${var.env}"]
-  }
-}
+# data "aws_subnet" "private_subnet_az1" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["bsp-private-subnet-az1-${var.env}"]
+#   }
+# }
 
-data "aws_subnet" "private_subnet_az2" {
-  filter {
-    name   = "tag:Name"
-    values = ["bsp-private-subnet-az2-${var.env}"]
-  }
-}
+# data "aws_subnet" "private_subnet_az2" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["bsp-private-subnet-az2-${var.env}"]
+#   }
+# }
 
-data "aws_subnet" "public_subnet_az1" {
-  filter {
-    name   = "tag:Name"
-    values = ["bsp-public-subnet-az1-${var.env}"]  
-  }
-}
+# data "aws_subnet" "public_subnet_az1" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["bsp-public-subnet-az1-${var.env}"]  
+#   }
+# }
 
-data "aws_subnet" "public_subnet_az2" {
-  filter {
-    name   = "tag:Name"
-    values = ["bsp-public-subnet-az2-${var.env}"]  
-  }
-}
-
-
-# Get current caller identity and region
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
-data "aws_eks_cluster_auth" "pw_eks" {
-  name = aws_eks_cluster.eks.name
-}
+# data "aws_subnet" "public_subnet_az2" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["bsp-public-subnet-az2-${var.env}"]  
+#   }
+# }
 
 
+# # Get current caller identity and region
+# data "aws_caller_identity" "current" {}
+# data "aws_region" "current" {}
+
+# data "aws_eks_cluster_auth" "pw_eks" {
+#   name = aws_eks_cluster.eks.name
+# }
 
 
-# Creating EKS Cluster
-resource "aws_eks_cluster" "eks" {
-  name     = "${var.project_name}-eks-cluster-${var.env}"
-  role_arn = var.master_role_arn
-  version  = var.eks_version
 
-  vpc_config {
-    subnet_ids              = [data.aws_subnet.private_subnet_az1.id, data.aws_subnet.private_subnet_az2.id]
-    endpoint_private_access = true
-    endpoint_public_access  = true
-    security_group_ids      = [var.eks_master_sg_id]
-  }
 
-  # Enable all log types
-  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+# # Creating EKS Cluster
+# resource "aws_eks_cluster" "eks" {
+#   name     = "${var.project_name}-eks-cluster-${var.env}"
+#   role_arn = var.master_role_arn
+#   version  = var.eks_version
 
-  access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
-    # authentication_mode = "API"
-    bootstrap_cluster_creator_admin_permissions = true
-  }
+#   vpc_config {
+#     subnet_ids              = [data.aws_subnet.private_subnet_az1.id, data.aws_subnet.private_subnet_az2.id]
+#     endpoint_private_access = true
+#     endpoint_public_access  = true
+#     security_group_ids      = [var.eks_master_sg_id]
+#   }
+
+#   # Enable all log types
+#   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+#   access_config {
+#     authentication_mode = "API_AND_CONFIG_MAP"
+#     # authentication_mode = "API"
+#     bootstrap_cluster_creator_admin_permissions = true
+#   }
   
-  # encryption_config {
-  #   provider {
-  #     key_arn = aws_kms_key.eks_secrets.arn
-  #   }
-  #   resources = ["secrets"]
-  # }
+#   # encryption_config {
+#   #   provider {
+#   #     key_arn = aws_kms_key.eks_secrets.arn
+#   #   }
+#   #   resources = ["secrets"]
+#   # }
 
-  # Ensure that CloudWatch log group is created before the EKS cluster
-  depends_on = [aws_cloudwatch_log_group.eks_cluster]
+#   # Ensure that CloudWatch log group is created before the EKS cluster
+#   depends_on = [aws_cloudwatch_log_group.eks_cluster]
 
-  tags = merge(
-    { "Name"    = "${var.project_name}-eks-cluster-${var.env}" },
-    var.map_tagging
-  )
+#   tags = merge(
+#     { "Name"    = "${var.project_name}-eks-cluster-${var.env}" },
+#     var.map_tagging
+#   )
   
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
-}
+#   # lifecycle {
+#   #   prevent_destroy = true
+#   # }
+# }
 
-# Create CloudWatch Log Group for EKS cluster logs
-resource "aws_cloudwatch_log_group" "eks_cluster" {
-  name              = "/aws/eks/${var.project_name}-cluster-${var.env}/cluster"
-  retention_in_days = 365
+# # Create CloudWatch Log Group for EKS cluster logs
+# resource "aws_cloudwatch_log_group" "eks_cluster" {
+#   name              = "/aws/eks/${var.project_name}-cluster-${var.env}/cluster"
+#   retention_in_days = 365
   
-  # kms_key_id  = data.aws_kms_key.cloudwatch-log-group.arn
+#   # kms_key_id  = data.aws_kms_key.cloudwatch-log-group.arn
   
-  tags = merge(
-    { "Name"    = "${var.project_name}-eks-cluster-logs-${var.env}" },
-    var.map_tagging
-  )
-}
+#   tags = merge(
+#     { "Name"    = "${var.project_name}-eks-cluster-logs-${var.env}" },
+#     var.map_tagging
+#   )
+# }
 
 
 
 
-########################################################################################################################################################################
+# ########################################################################################################################################################################
 
-########################################################### eks addons ################################################################################################
+# ########################################################### eks addons ################################################################################################
 
-########################################################################################################################################################################
+# ########################################################################################################################################################################
 
-data "aws_eks_addon_version" "vpc-cni-default" {
-  addon_name         = "vpc-cni"
-  kubernetes_version = aws_eks_cluster.eks.version
-}
+# data "aws_eks_addon_version" "vpc-cni-default" {
+#   addon_name         = "vpc-cni"
+#   kubernetes_version = aws_eks_cluster.eks.version
+# }
 
-resource "aws_eks_addon" "vpc-cni" {
-  addon_name        = "vpc-cni"
-  addon_version     = data.aws_eks_addon_version.vpc-cni-default.version
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-  cluster_name      = aws_eks_cluster.eks.name
+# resource "aws_eks_addon" "vpc-cni" {
+#   addon_name        = "vpc-cni"
+#   addon_version     = data.aws_eks_addon_version.vpc-cni-default.version
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+#   cluster_name      = aws_eks_cluster.eks.name
   
-  configuration_values = jsonencode({
-    "enableNetworkPolicy" = "true"
-  })
-}
+#   configuration_values = jsonencode({
+#     "enableNetworkPolicy" = "true"
+#   })
+# }
 
 
 
 
 
-data "aws_eks_addon_version" "kube-proxy-default" {
-  addon_name         = "kube-proxy"
-  kubernetes_version = aws_eks_cluster.eks.version
-}
+# data "aws_eks_addon_version" "kube-proxy-default" {
+#   addon_name         = "kube-proxy"
+#   kubernetes_version = aws_eks_cluster.eks.version
+# }
 
-resource "aws_eks_addon" "kube-proxy" {
-  addon_name        = "kube-proxy"
-  addon_version     = data.aws_eks_addon_version.kube-proxy-default.version
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-  cluster_name      = aws_eks_cluster.eks.name
+# resource "aws_eks_addon" "kube-proxy" {
+#   addon_name        = "kube-proxy"
+#   addon_version     = data.aws_eks_addon_version.kube-proxy-default.version
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+#   cluster_name      = aws_eks_cluster.eks.name
 
-}
-
-
+# }
 
 
 
-data "aws_eks_addon_version" "coredns-default" {
-  addon_name         = "coredns"
-  kubernetes_version = aws_eks_cluster.eks.version
-}
 
-resource "aws_eks_addon" "coredns" {
-  addon_name        = "coredns"
-  addon_version     = data.aws_eks_addon_version.coredns-default.version
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-  cluster_name      = aws_eks_cluster.eks.name
 
-}
+# data "aws_eks_addon_version" "coredns-default" {
+#   addon_name         = "coredns"
+#   kubernetes_version = aws_eks_cluster.eks.version
+# }
 
-#######################################################################
-#oidc 
+# resource "aws_eks_addon" "coredns" {
+#   addon_name        = "coredns"
+#   addon_version     = data.aws_eks_addon_version.coredns-default.version
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+#   cluster_name      = aws_eks_cluster.eks.name
 
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
-}
+# }
 
-resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+# #######################################################################
+# #oidc 
+
+# data "tls_certificate" "eks" {
+#   url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+# }
+
+# resource "aws_iam_openid_connect_provider" "eks" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+#   url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
   
-  tags = var.map_tagging
-}
+#   tags = var.map_tagging
+# }
 
 
 
-#######################################################################
+# #######################################################################
 
-# EBS CSI Driver addon
-data "aws_eks_addon_version" "ebs-csi-driver-default" {
-  addon_name         = "aws-ebs-csi-driver"
-  kubernetes_version = aws_eks_cluster.eks.version
-}
+# # EBS CSI Driver addon
+# data "aws_eks_addon_version" "ebs-csi-driver-default" {
+#   addon_name         = "aws-ebs-csi-driver"
+#   kubernetes_version = aws_eks_cluster.eks.version
+# }
 
 
-resource "aws_eks_addon" "ebs-csi-driver" {
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = data.aws_eks_addon_version.ebs-csi-driver-default.version
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-  cluster_name             = aws_eks_cluster.eks.name
-  service_account_role_arn = var.ebs_csi_driver_role_arn
-}
+# resource "aws_eks_addon" "ebs-csi-driver" {
+#   addon_name               = "aws-ebs-csi-driver"
+#   addon_version            = data.aws_eks_addon_version.ebs-csi-driver-default.version
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   resolve_conflicts_on_update = "OVERWRITE"
+#   cluster_name             = aws_eks_cluster.eks.name
+#   service_account_role_arn = var.ebs_csi_driver_role_arn
+# }
 
 
 
